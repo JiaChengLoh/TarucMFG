c08ec8f71bb3c6a7d1c1b0777c69d5604a61803e
JY <tanjy-wp17@student.tarc.edu.my>
2020-11-27 20:30:28 +0800

    create folders, launcher icon

--- app/src/main/java/com/example/mrfarmergrocer/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/mrfarmergrocer/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.mrfarmergrocer
+package com.example.mrfarmergrocer.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mrfarmergrocer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
