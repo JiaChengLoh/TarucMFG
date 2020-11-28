3e0521696366723626e7cf9b19b3777cafbdc36d
Licz728 <vincentli_73@hotmail.com>
2020-11-28 17:15:41 +0800

    register successful

--- app/src/main/java/com/example/mrfarmergrocer/ui/activities/BaseActivity.kt
+++ app/src/main/java/com/example/mrfarmergrocer/ui/activities/BaseActivity.kt
@@ -1,15 +1,17 @@
 package com.example.mrfarmergrocer.ui.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
 import com.example.mrfarmergrocer.R
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.dialog_progress.*
 
 open class BaseActivity : AppCompatActivity() {
 
-    // TODO Step 4: Create a function to show the success and error messages in snack bar component.
-    // START
+    private lateinit var mProgressDialog: Dialog
+
     /**
      * A function to show the success and error messages in snack bar component.
      */
@@ -35,5 +37,24 @@ open class BaseActivity : AppCompatActivity() {
         }
         snackBar.show()
     }
-    // END
+
+    fun showProgressDialog(text: String) {
+        mProgressDialog = Dialog(this)
+
+        /*Set the screen content from a layout resource.
+        The resource will be inflated, adding all top-level views to the screen.*/
+        mProgressDialog.setContentView(R.layout.dialog_progress)
+
+        mProgressDialog.tv_progress_text.text = text
+
+        mProgressDialog.setCancelable(false)
+        mProgressDialog.setCanceledOnTouchOutside(false)
+
+        //Start the dialog and display it on screen.
+        mProgressDialog.show()
+    }
+
+    fun hideProgressDialog() {
+        mProgressDialog.dismiss()
+    }
 }
