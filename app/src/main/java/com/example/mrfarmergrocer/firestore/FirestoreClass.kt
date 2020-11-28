3648b55f7adae6a754efc034d9309cdd5cab46e8
Andy Chow <andycsk-wp17@student.tarc.edu>
2020-11-28 18:20:05 +0800

    firestore

---
+++ app/src/main/java/com/example/mrfarmergrocer/firestore/FirestoreClass.kt
@@ -0,0 +1,8 @@
+package com.example.mrfarmergrocer.firestore
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+class FirestoreClass {
+    private val mFireStore = FirebaseFirestore.getInstance()
+
+}
