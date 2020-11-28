02e526faf409c01f84ea44f0d06cb0f004fc6547
JiaChengLoh <67746528+JiaChengLoh@users.noreply.github.com>
2020-11-28 09:02:04 +0800

    LoginScreen

--- app/src/main/java/com/example/mrfarmergrocer/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/mrfarmergrocer/ui/activities/LoginActivity.kt
@@ -1,19 +1,16 @@
 package com.example.mrfarmergrocer.ui.activities
 
-import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.WindowInsets
 import android.view.WindowManager
-import com.example.mrfarmergrocer.ui.activities.MainActivity
 import com.example.mrfarmergrocer.R
 
-class SplashActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_login)
 
         @Suppress("DEPRECATION")
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
@@ -24,17 +21,5 @@ class SplashActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
-
-        @Suppress("DEPRECATION")
-        Handler().postDelayed(
-            {
-                // Launch the Main Activity
-                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-                finish() // Call this when your activity is done and should be closed.
-            },
-            2500
-        ) // Here we pass the delay time in milliSeconds after which the splash activity will disappear.
-
-        // END
     }
 }
