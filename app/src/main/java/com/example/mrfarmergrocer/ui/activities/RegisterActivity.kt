f088612c4b7266732619b827daad6f0972535fa1
Licz728 <vincentli_73@hotmail.com>
2020-11-28 15:29:09 +0800

    commit

--- app/src/main/java/com/example/mrfarmergrocer/ui/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/mrfarmergrocer/ui/activities/RegisterActivity.kt
@@ -3,22 +3,13 @@ package com.example.mrfarmergrocer.ui.activities
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.text.TextUtils
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mrfarmergrocer.R
 
-class RegisterActivity : BaseActivity() {
-
-    var btn_register: Button = findViewById(R.id.btn_register)
-    var toolbar_register_activity: Toolbar = findViewById(R.id.toolbar_register_activity)
-    var et_confirm_password: EditText = findViewById(R.id.et_confirm_password)
-    var et_email: EditText = findViewById(R.id.et_email)
-    var et_first_name: EditText = findViewById(R.id.et_first_name)
-    var et_last_name: EditText = findViewById(R.id.et_last_name)
-    var et_password: EditText = findViewById(R.id.et_password)
-    var cb_terms_and_condition: CheckBox = findViewById(R.id.cb_terms_and_condition)
+class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,75 +25,13 @@ class RegisterActivity : BaseActivity() {
             )
         }
 
-        //setupActionBar()
-
         val tv_login: TextView = findViewById(R.id.tv_login)
         tv_login.setOnClickListener {
 
             // Launch the register screen when the user clicks on the text.
             val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
             startActivity(intent)
-        }
-
-        btn_register.setOnClickListener {
-
-            validateRegisterDetails()
-        }
-    }
-
-    //private fun setupActionBar() {
-
-        //setSupportActionBar(findViewById(R.id.toolbar_register_activity)
-
-        //val actionBar = supportActionBar
-        //if (actionBar != null) {
-            //actionBar.setDisplayHomeAsUpEnabled(true)
-            //actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24)
-        //}
-
-        //toolbar_register_activity.setNavigationOnClickListener { onBackPressed() }
-    //}
-
-    private fun validateRegisterDetails(): Boolean {
-        return when {
-            TextUtils.isEmpty(et_first_name.text.toString().trim { it <= ' ' }) -> {
-                showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name), true)
-                false
-            }
-
-            TextUtils.isEmpty(et_last_name.text.toString().trim { it <= ' ' }) -> {
-                showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), true)
-                false
-            }
-
-            TextUtils.isEmpty(et_email.text.toString().trim { it <= ' ' }) -> {
-                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
-                false
-            }
-
-            TextUtils.isEmpty(et_password.text.toString().trim { it <= ' ' }) -> {
-                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
-                false
-            }
-
-            TextUtils.isEmpty(et_confirm_password.text.toString().trim { it <= ' ' }) -> {
-                showErrorSnackBar(resources.getString(R.string.err_msg_enter_confirm_password), true)
-                false
-            }
-
-            et_password.text.toString().trim { it <= ' ' } != et_confirm_password.text.toString()
-                .trim { it <= ' ' } -> {
-                showErrorSnackBar(resources.getString(R.string.err_msg_password_and_confirm_password_mismatch), true)
-                false
-            }
-            !cb_terms_and_condition.isChecked -> {
-                showErrorSnackBar(resources.getString(R.string.err_msg_agree_terms_and_condition), true)
-                false
-            }
-            else -> {
-                showErrorSnackBar("Your details are valid.", false)
-                true
-            }
+            finish()
         }
     }
 }
