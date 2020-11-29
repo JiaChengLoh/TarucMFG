edc4f7757a3e8a4483ae9bacd2cdf746b327dc3f
JiaChengLoh <67746528+JiaChengLoh@users.noreply.github.com>
2020-11-29 17:04:46 +0800

    Product Activity error

--- app/src/main/java/com/example/mrfarmergrocer/ui/activities/ProductsActivity.kt
+++ app/src/main/java/com/example/mrfarmergrocer/ui/activities/ProductsActivity.kt
@@ -2,20 +2,16 @@ package com.example.mrfarmergrocer.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import android.widget.ImageView
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mrfarmergrocer.R
 import com.example.mrfarmergrocer.firestore.FirestoreClass
 import com.example.mrfarmergrocer.models.Product
+import com.example.mrfarmergrocer.adapter.MyProductsListAdapter
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_products.*
 import kotlinx.android.synthetic.main.bottom_nav_view.*
-import com.example.mrfarmergrocer.utils.Constants
 
 
 class ProductsActivity : BaseActivity() {
@@ -62,11 +58,6 @@ class ProductsActivity : BaseActivity() {
         // Hide Progress dialog.
         hideProgressDialog()
 
-        for(i in productsList){
-            Log.i("Price", i.price)
-        }
-
-        /*
         if (productsList.size > 0) {
             rv_my_product_items.visibility = View.VISIBLE
             tv_no_products_found.visibility = View.GONE
@@ -75,13 +66,13 @@ class ProductsActivity : BaseActivity() {
             rv_my_product_items.setHasFixedSize(true)
 
             val adapterProducts =
-                    MyProductsListAdapter(requireActivity(), productsList, this@ProductsFragment)
+                    MyProductsListAdapter(RequireActivity(), productsList, this@ProductsActivity)
             rv_my_product_items.adapter = adapterProducts
         } else {
             rv_my_product_items.visibility = View.GONE
             tv_no_products_found.visibility = View.VISIBLE
         }
-        */
+
 
     }
 
