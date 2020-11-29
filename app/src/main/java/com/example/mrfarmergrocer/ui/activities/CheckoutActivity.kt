4743c6a42c6995c192cb51d0882f3f9200de810b
Andy Chow <andycsk-wp17@student.tarc.edu>
2020-11-30 00:41:44 +0800

    checkout

---
+++ app/src/main/java/com/example/mrfarmergrocer/ui/activities/CheckoutActivity.kt
@@ -0,0 +1,64 @@
+package com.example.mrfarmergrocer.ui.activities
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mrfarmergrocer.R
+import com.example.mrfarmergrocer.models.Address
+import com.example.mrfarmergrocer.utils.Constants
+import kotlinx.android.synthetic.main.activity_checkout.*
+
+
+/**
+ * A CheckOut activity screen.
+ */
+class CheckoutActivity : AppCompatActivity() {
+
+    private var mAddressDetails: Address? = null
+    /**
+     * This function is auto created by Android when the Activity Class is created.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
+        super.onCreate(savedInstanceState)
+        // This is used to align the xml view to this class
+        setContentView(R.layout.activity_checkout)
+        setupActionBar()
+
+        if (intent.hasExtra(Constants.EXTRA_SELECTED_ADDRESS)) {
+            mAddressDetails =
+                intent.getParcelableExtra<Address>(Constants.EXTRA_SELECTED_ADDRESS)!!
+        }
+
+        if (mAddressDetails != null) {
+            tv_checkout_address_type.text = mAddressDetails?.type
+            tv_checkout_full_name.text = mAddressDetails?.name
+            tv_checkout_address.text = "${mAddressDetails!!.address}, ${mAddressDetails!!.zipCode}"
+            tv_checkout_additional_note.text = mAddressDetails?.additionalNote
+
+            if (mAddressDetails?.otherDetails!!.isNotEmpty()) {
+                tv_checkout_other_details.text = mAddressDetails?.otherDetails
+            }
+
+            tv_checkout_mobile_number.text = mAddressDetails?.mobileNumber
+        }
+
+    }
+
+    /**
+     * A function for actionBar Setup.
+     */
+    private fun setupActionBar() {
+
+        setSupportActionBar(toolbar_checkout_activity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+        }
+
+        toolbar_checkout_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+    // END
+}
